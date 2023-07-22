8adf034410b75c22a4a6505e43b559b41533798d
David McArthur <dmcarthur86@gmail.com>
2023-07-22 11:48:29 +1000

    tested render, ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-    </div>
-  );
-}
-
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      )
+    };
 export default App;
