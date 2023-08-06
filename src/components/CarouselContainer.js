0a79149d80cdec6b18f57d02d4f8fca2126c18ed
David McArthur <dmcarthur86@gmail.com>
2023-08-06 11:45:07 +1000

    fixed background height issue on all sizes, removed extra empty space

--- src/components/CarouselContainer.js
+++ src/components/CarouselContainer.js
@@ -2,8 +2,8 @@ import React from "react";
 import CustomCarousel from "./CustomCarousel";
 
 const CarouselContainer = ({ title, text, items, subtitle }) => (
-  <div className="mx-auto my-auto text-white sm:w-full md:w-3/4 lg:w-1/2 pt-4 md:pt-10">
-    <div className="bg-black/70 px-6 rounded-2xl relative pb-20 sm:pb-32 md:pb-12">
+  <div className="mx-auto text-white sm:w-full  md:w-3/4 lg:w-1/2 pt-4 md:pt-10">
+    <div className="bg-black/70 px-6 rounded-2xl relative pb-12 ">
       <div className="flex flex-col md:flex-row items-start">
         <div className="w-full md:w-1/2 h-auto md:h-24">
           <h1 className="text-4xl text-white mb-2 pt-4">{title}</h1>
